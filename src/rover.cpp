c1534aec54450afba4755c6d8715169ce8390e76
matt <matthewk.rydalch@gmail.com>
2019-06-27 14:37:35 -0600

    "got fixed rtk data from this"

--- src/rover.cpp
+++ src/rover.cpp
@@ -1,6 +1,10 @@
+#include <iostream>
+#include <fstream>
 #include <signal.h>
 
 #include "UBLOX/ublox.h"
+
+using namespace std;
 
 int i = 1;
 
@@ -23,6 +27,7 @@ void relposned_callback(uint8_t cls, uint8_t type, const UBX::UBX_message_t& in_
     {
         printf("No RTK \n");
         RTK_flag = 0;
+        i++;
     }
     if (RTK_flag == 1)
     {
@@ -32,14 +37,21 @@ void relposned_callback(uint8_t cls, uint8_t type, const UBX::UBX_message_t& in_
             printf(" , Floating \n");
         else if (msg.flags && 0b000010000)
             printf(" , Fixed \n");
+        if (msg.flags && 0b000000100)
+            printf("valid relative position components \n");
         printf("%d relative t: %d, NED: %d, %d, %d, Distance: %d\n",
                i,
                msg.iTow/1000,
                msg.relPosN, msg.relPosE, msg.relPosD, msg.relPosLength);
+        ofstream myfile;
+        myfile.open ("../textfiles/9test_3ft_little_move/data.txt", ios::app);
+        myfile <<  msg.iTow
+               << " " << msg.relPosN << " " << msg.relPosE << " " << msg.relPosD
+               << " " << msg.relPosLength << " \n";
+        myfile.close();
+        i++;
     }
-
     fflush(stdout); // Will now print everything in the stdout buffer
-    i++;
 }
 
 int main(int argc, char** argv)
