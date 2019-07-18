d92902dc234da0cc8cfc4084e169afc16378a60e
James Jackson <superjax08@gmail.com>
2019-07-18 14:33:20 -0600

    removed RTCM parsing

--- include/UBLOX/parsers/rtcm.h
+++ include/UBLOX/parsers/rtcm.h
@@ -24,38 +24,38 @@ public:
     union RTCM_message_t
     {
         uint8_t buf[BUFFER_SIZE];
-        RTCM1001 rtcm1001;
-        RTCM1002 rtcm1002;
-        RTCM1003 rtcm1003;
-        RTCM1004 rtcm1004;
-        RTCM1005 rtcm1005;
-        RTCM1006 rtcm1006;
-        RTCM1009 rtcm1009;
-        RTCM1010 rtcm1010;
-        RTCM1011 rtcm1011;
-        RTCM1012 rtcm1012;
-        RTCM1019 rtcm1019;
-        RTCM1020 rtcm1020;
+//        RTCM1001 rtcm1001;
+//        RTCM1002 rtcm1002;
+//        RTCM1003 rtcm1003;
+//        RTCM1004 rtcm1004;
+//        RTCM1005 rtcm1005;
+//        RTCM1006 rtcm1006;
+//        RTCM1009 rtcm1009;
+//        RTCM1010 rtcm1010;
+//        RTCM1011 rtcm1011;
+//        RTCM1012 rtcm1012;
+//        RTCM1019 rtcm1019;
+//        RTCM1020 rtcm1020;
 
-        BitField<0,8> hdr;
-        BitField<8,6> res;
-        BitField<14,10> len;
-        BitField<24,12> type;
+//        BitField<0,8> hdr;
+//        BitField<8,6> res;
+//        BitField<14,10> len;
+//        BitField<24,12> type;
 
-        MSM4<GPS> rtcm1074;
-        MSM4<Galileo> rtcm1084;
-        MSM4<Glonass> rtcm1094;
-        MSM4<Beidou> rtcm1124;
-        MSM5<GPS> rtcm1075;
-        MSM5<Galileo> rtcm1085;
-        MSM5<Glonass> rtcm1095;
-        MSM5<Beidou> rtcm1125;
-        MSM7<GPS> rtcm1077;
-        MSM7<Galileo> rtcm1087;
-        MSM7<Glonass> rtcm1097;
-        MSM7<Beidou> rtcm1127;
+//        MSM4<GPS> rtcm1074;
+//        MSM4<Galileo> rtcm1084;
+//        MSM4<Glonass> rtcm1094;
+//        MSM4<Beidou> rtcm1124;
+//        MSM5<GPS> rtcm1075;
+//        MSM5<Galileo> rtcm1085;
+//        MSM5<Glonass> rtcm1095;
+//        MSM5<Beidou> rtcm1125;
+//        MSM7<GPS> rtcm1077;
+//        MSM7<Galileo> rtcm1087;
+//        MSM7<Glonass> rtcm1097;
+//        MSM7<Beidou> rtcm1127;
 
-        RTCM_message_t() {}
+//        RTCM_message_t() {}
     } in_buffer_;
     uint32_t canary_ = 0xCAFEBABE;
 
@@ -100,14 +100,14 @@ public:
     } parse_state_t;
     parse_state_t parse_state_;
 
-    typedef std::function<void(uint8_t, const RTCM_message_t&)> rtcm_cb;
-    struct callback_t
-    {
-        uint16_t rtcm_msg;
-        rtcm_cb cb;
-    };
-    void registerCallback(uint16_t msg_id, rtcm_cb cb);
-    std::vector<callback_t> callbacks_;
+//    typedef std::function<void(uint8_t, const RTCM_message_t&)> rtcm_cb;
+//    struct callback_t
+//    {
+//        uint16_t rtcm_msg;
+//        rtcm_cb cb;
+//    };
+//    void registerCallback(uint16_t msg_id, rtcm_cb cb);
+//    std::vector<callback_t> callbacks_;
 
     typedef std::function<void(uint8_t*, size_t)> buffer_cb;
     void registerBufferCallback(buffer_cb cb);
