b8969fc951bcb2f1ea74594e625245b198a80cec
James Jackson <superjax08@gmail.com>
2019-07-25 18:12:08 -0600

    started ROS bindings to raw data

--- include/UBLOX/ublox_ros.h
+++ include/UBLOX/ublox_ros.h
@@ -7,10 +7,14 @@
 
 #include "UBLOX/ublox.h"
 
-#include "ublox/GNSS.h"
+#include "ublox/PosVelEcef.h"
 #include "ublox/PositionVelocityTime.h"
 #include "ublox/RelPos.h"
-#include "ublox/SVIN.h"
+#include "ublox/SurveyStatus.h"
+#include "ublox/Ephemeris.h"
+#include "ublox/GlonassEphemeris.h"
+#include "ublox/Observation.h"
+#include "ublox/ObsVec.h"
 
 namespace ublox_ros
 {
@@ -28,11 +32,14 @@ private:
     ros::NodeHandle nh_private_;
 
     ros::Publisher pvt_pub_;
-    ros::Publisher svin_pub_;
+    ros::Publisher survey_status_pub_;
     ros::Publisher relpos_pub_;
-    ros::Publisher gnss_pub_;
+    ros::Publisher ecef_pub_;
     ros::Publisher nav_sat_fix_pub_;
     ros::Publisher nav_sat_status_pub_;
+    ros::Publisher eph_pub_;
+    ros::Publisher geph_pub_;
+    ros::Publisher obs_pub_;
 
     void pvtCB(const ublox::NAV_PVT_t& msg);
     void relposCB(const ublox::NAV_RELPOSNED_t& msg);
@@ -40,10 +47,14 @@ private:
     void velECEFCB(const ublox::NAV_VELECEF_t& msg);
     void svinCB(const ublox::NAV_SVIN_t& msg);
 
+    void obsCB(const ublox::RXM_RAWX_t& msg);
+    void ephCB(const Ephemeris& eph);
+    void gephCB(const GlonassEphemeris& eph);
+
     uint32_t pos_tow_;
     uint32_t vel_tow_;
     uint32_t pvt_tow_;
-    ublox::GNSS ecef_msg_;
+    ublox::PosVelEcef ecef_msg_;
 };
 
 }
